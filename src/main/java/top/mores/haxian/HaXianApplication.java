972523177a6d48e4a9560b82d5b4443a3f9e7680
Do0oMores <ws2004421@outlook.com>
2024-05-25 17:21:03 +0800

    init

---
+++ src/main/java/top/mores/haxian/HaXianApplication.java
@@ -0,0 +1,13 @@
+package top.mores.haxian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaXianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaXianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/mores/haxian/HaXianApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mores.haxian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaXianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
