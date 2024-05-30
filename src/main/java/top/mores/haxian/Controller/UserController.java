be2ace3808b9a29059368275df5d84fa787c199a
Do0oMores <do0omores@outlook.com>
2024-05-31 00:21:59 +0800

    完成登录后端处理

--- src/main/java/top/mores/haxian/Controller/UserController.java
+++ src/main/java/top/mores/haxian/Controller/UserController.java
@@ -2,6 +2,8 @@ package top.mores.haxian.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -40,25 +43,30 @@ public class UserController {
     //检查密码
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     @ResponseBody
-    public void getUserPassword(@RequestParam("userName") String userName,
-                                @RequestParam("userPassword") String userPassword){
+    public ResponseEntity<Map<String, Object>> getUserPassword(@RequestParam("userName") String userName,
+                                                               @RequestParam("userPassword") String userPassword){
         String sql = "select pwd from users where name = ?";
         String sql1="select is_admin from users where name = ?";
+        Map<String,Object> response=new HashMap<>();
         try {
             String password = jdbcTemplate.queryForObject(sql, new Object[]{userName}, String.class);
             int isAdmin=jdbcTemplate.queryForObject(sql1,new Object[]{userName},int.class);
             if (userPassword.equals(password)) {
-                System.out.println("登录成功");
+                response.put("code",200);
+                response.put("msg","登录成功");
                 if (isAdmin==1){
-                    System.out.println("进入管理界面");
+                    response.put("role","admin");
                 }else {
-                    System.out.println("进入用户界面");
+                    response.put("role","user");
                 }
             } else {
-                System.out.println(userPassword);
+                response.put("code",401);
+                response.put("msg","密码错误");
             }
         } catch (EmptyResultDataAccessException e) {
-            System.out.println(22);
+            response.put("code",404);
+            response.put("msg","用户不存在");
         }
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
