8a6899636eabb9614579a7c41f50ec8ac543291c
Do0oMores <do0omores@outlook.com>
2024-05-30 11:13:51 +0800

    准备前后端分离

--- src/main/java/top/mores/haxian/Controller/UserController.java
+++ src/main/java/top/mores/haxian/Controller/UserController.java
@@ -2,11 +2,13 @@ package top.mores.haxian.Controller;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.util.List;
@@ -34,30 +36,29 @@ public class UserController {
     public List<Map<String,Object>> getAllUsers(){
         String sql="select * from users";
         List<Map<String,Object>> list=jdbcTemplate.queryForList(sql);
-        try {
-            jdbcTemplate.execute("insert into users (id, name, pwd, phonenumber) values (2, '李四', '3344', 13300000000)");
-        } catch (DataAccessException exception) {
-            throw new RuntimeException("数据库报错");
-        }
+//        try {
+//            jdbcTemplate.execute("insert into users (id, name, pwd, phonenumber) values (2, '李四', '3344', 13300000000)");
+//        } catch (DataAccessException exception) {
+//            throw new RuntimeException("数据库报错");
+//        }
         return list;
     }
 
     //检查密码
-    @RequestMapping(value = "/getUserPassword", method = RequestMethod.POST)
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
     @ResponseBody
-    public void getUserPassword(@RequestParam("userName") String username,
-                                @RequestParam("userPassword") String userPassword,
-                                HttpServletResponse response)throws IOException {
+    public void getUserPassword(@RequestParam("userName") String userName,
+                                @RequestParam("userPassword") String userPassword){
         String sql = "select pwd from users where name = ?";
         try {
-            String password = jdbcTemplate.queryForObject(sql, new Object[]{username}, String.class);
+            String password = jdbcTemplate.queryForObject(sql, new Object[]{userName}, String.class);
             if (userPassword.equals(password)) {
-                response.sendRedirect("http://localhost:8080/HaXianMain.jsp");
+                System.out.println(userName);
             } else {
-                response.sendRedirect("http://localhost:8080/UserLoginFailed.jsp");
+                System.out.println(userPassword);
             }
         } catch (EmptyResultDataAccessException e) {
-            response.sendRedirect("http://localhost:8080/UserLoginFailed.jsp");
+            System.out.println(22);
         }
     }
 }
