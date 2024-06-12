65cdf1b24064eb96115b16b03086acc4d88993d9
Do0oMores <do0omores@outlook.com>
2024-06-12 10:29:29 +0800

    管理查询预约订单功能

--- src/main/java/top/mores/haxian/service/AdminService/queryReservation.java
+++ src/main/java/top/mores/haxian/service/AdminService/queryReservation.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,29 +14,35 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class queryProductsInformation {
+public class queryReservation {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    @GetMapping("/products")
-    public ResponseEntity<Map<String, Object>> productsInformation(@RequestParam("productName") String productName) {
-        String sql =
-                "select product_id,name,description,price,stock,origin,production_date,support,create_time,shelf_life from products where name =?";
+    @GetMapping("/reservation")
+    public ResponseEntity<Map<String, Object>> reservationData() {
+        String sql = "select * from reservations";
+        String sql1 = "select name from users where id = ?";
+        String sql2 = "select name from products where product_id = ?";
         Map<String, Object> response = new HashMap<>();
         try {
-            Map<String, Object> products = jdbcTemplate.queryForMap(sql, productName);
+            Map<String, Object> reservations = jdbcTemplate.queryForMap(sql);
             response.put("code", 200);
             response.put("msg", "查询成功");
+            String userName = jdbcTemplate.queryForObject(sql1, new Object[]{reservations.get("userId")}, String.class);
+            String productName=jdbcTemplate.queryForObject(sql2,new Object[]{reservations.get("productId")},String.class);
+            reservations.put("userId",userName);
+            reservations.put("productId",productName);
             List<Map<String, Object>> data = new ArrayList<>();
-            data.add(products);
+            data.add(reservations);
             response.put("Data", data);
         } catch (EmptyResultDataAccessException e) {
             response.put("code", 404);
-            response.put("msg", "没有名为" + productName + "的商品");
+            response.put("msg", "没有查询到预约信息");
         } catch (Exception e) {
+            //e.printStackTrace();
             response.put("code", 500);
-            response.put("msg", "服务器错误");
+            response.put("msg", "服务器内部错误");
         }
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
