fde517f13c7f77b11ad04e4e65868ae48210d998
Do0oMores <do0omores@outlook.com>
2024-06-12 11:56:37 +0800

    增加订单查询功能，更正预约查询错误

--- src/main/java/top/mores/haxian/service/AdminService/queryUserOrders.java
+++ src/main/java/top/mores/haxian/service/AdminService/queryUserOrders.java
@@ -13,43 +13,48 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class queryReservation {
+public class queryUserOrders {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    @GetMapping("/reservation")
-    public ResponseEntity<Map<String, Object>> reservationData() {
-        String sql = "select * from reservations";
+    @GetMapping("/orders")
+    public ResponseEntity<Map<String, Object>> userOrders() {
+        String sql = "select * from orders";
         String sql1 = "select name from users where id = ?";
         String sql2 = "select name from products where product_id = ?";
         Map<String, Object> response = new HashMap<>();
         try {
-            List<Map<String, Object>> reservations = jdbcTemplate.queryForList(sql);
-            if (reservations.isEmpty()){
-                response.put("code",404);
-                response.put("msg","没有查询到预约信息");
-            }else {
-                for (Map<String,Object> reservation : reservations){
-                    String userId=reservation.get("userId").toString();
+            // 查询orders表
+            List<Map<String, Object>> orders = jdbcTemplate.queryForList(sql);
+            if (orders.isEmpty()) {
+                response.put("code", 404);
+                response.put("msg", "没有查到消费记录");
+            } else {
+                for (Map<String, Object> order : orders) {
+                    // 从users表获取userName
+                    String userId = order.get("user_id").toString();
                     String userName = jdbcTemplate.queryForObject(sql1, new Object[]{userId}, String.class);
-                    reservation.put("userId",userName);
-                    String productId=reservation.get("productId").toString();
-                    String productName=jdbcTemplate.queryForObject(sql2,new Object[]{productId},String.class);
-                    reservation.put("productId",productName);
+                    order.put("user_name", userName);
+
+                    // 从product表获取productName
+                    String productId = order.get("product_id").toString();
+                    String productName = jdbcTemplate.queryForObject(sql2, new Object[]{productId}, String.class);
+                    order.put("product_name", productName);
                 }
                 response.put("code", 200);
                 response.put("msg", "查询成功");
-                response.put("Data",reservations);
+                response.put("Data", orders);
             }
         } catch (EmptyResultDataAccessException e) {
             response.put("code", 404);
-            response.put("msg", "没有查询到预约信息");
+            response.put("msg", "没有查到消费记录");
         } catch (Exception e) {
             //e.printStackTrace();
             response.put("code", 500);
-            response.put("msg", "服务器内部错误");
+            response.put("msg", "服务器错误");
         }
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
 }
